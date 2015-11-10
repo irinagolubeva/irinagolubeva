707c56d03517ee389279fd092a4a671ef53ec3b6
Artem Zhukov <artzhuchka@gmail.com>
2015-11-10 20:40:46 +0300

    add github authorization to deploy task, fix travis config

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,17 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
+var env = process.env;
+
 gulp.task('deploy', function() {
-	return gulp.src('./build/**/*')
-		.pipe(ghPages({
-			branch: 'master',
-			message: 'Deploy to Github Pages',
-			remoteUrl: 'git@github.com:irinagolubeva/irinagolubeva.github.io.git'
-		}));
+	var ghPagesOptions = {
+		branch: 'master',
+		message: 'Deploy to Github Pages'
+	};
+
+	if (env.GH_TOKEN && env.GH_REF) {
+		ghPagesOptions.remoteUrl = 'https://' + env.GH_TOKEN + '@' + env.GH_REF;
+	}
+
+	return gulp.src('./build/**/*').pipe(ghPages(ghPagesOptions));
 });
